b90769c844f381e41e22e7728eabc03a074ce1d8
harispy2000 <harispy2000@gmail.com>
2026-09-25 09:52:42 +0530

    Add week 6 classes and objects problems

---
+++ week_6/m5_company/Employee.java
@@ -0,0 +1,42 @@
+package week_6.m5_company;
+
+public class Employee {
+
+    String empName;
+    double salary;
+
+    static String companyName =
+        "Bright Horizon Technologies";
+
+    static int employeeCount = 0;
+
+    public Employee(String empName, double salary) {
+
+        this.empName = empName;
+        this.salary = salary;
+
+        employeeCount++;
+    }
+
+    public static void printCompanyInfo() {
+
+        System.out.println(companyName);
+        System.out.println(
+            "Employees on record: " + employeeCount
+        );
+    }
+
+    public static void main(String[] args) {
+
+        Employee employee1 =
+            new Employee("Divya", 65000);
+
+        Employee employee2 =
+            new Employee("Arjun", 50000);
+
+        Employee employee3 =
+            new Employee("Priya", 55000);
+
+        Employee.printCompanyInfo();
+    }
+}
